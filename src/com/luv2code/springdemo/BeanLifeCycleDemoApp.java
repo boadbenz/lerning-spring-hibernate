de5be643f87f3496cf285f7869063cc0d3d5ffe9
pattaraphon.ban <manaya002@gmail.com>
2018-03-20 04:28:46 +0700

    Practice Coding again

--- src/com/luv2code/springdemo/BeanLifeCycleDemoApp.java
+++ src/com/luv2code/springdemo/BeanLifeCycleDemoApp.java
@@ -4,9 +4,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class BeanLifeCycleDemoApp {
     public static void main(String[] args){
-        ClassPathXmlApplicationContext context
-                = new ClassPathXmlApplicationContext("beanLifeCycle-applicationContext.xml");
 
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("beanLifeCycle-applicationContext.xml");
 
         Coach theCoach = context.getBean("myCoach" , Coach.class);
 
--- src/com/luv2code/springdemo/BeanScopeDemoApp.java
+++ src/com/luv2code/springdemo/BeanScopeDemoApp.java
@@ -4,21 +4,21 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class BeanScopeDemoApp {
     public static void main(String[] args){
-        ClassPathXmlApplicationContext context
-                = new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
 
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
 
         Coach theCoach = context.getBean("myCoach" , Coach.class);
 
-        Coach alphaCoach = context.getBean("myCoach" , Coach.class);
+        Coach myCoach = context.getBean("myCoach" , Coach.class);
 
-        boolean result = (theCoach == alphaCoach);
+        boolean isSameBeans = (theCoach == myCoach);
 
-        System.out.println("\nPointing to the same object: " + result);
+        System.out.println("is same beans : " + isSameBeans);
 
-        System.out.println("\nMemory location for theCoach: " + theCoach);
+        System.out.println("position bean theCoach : " + theCoach);
 
-        System.out.println("\nMemory location for alphaCoach: " + alphaCoach + "\n");
+        System.out.println("position bean myCoach : " + myCoach);
 
         context.close();
     }
